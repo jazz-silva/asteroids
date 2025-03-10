9bcf496cc36818c143ad5f7784da0fdfdaada806
José <ja.zzsilva111@gmail.com>
2025-03-10 20:30:20 -0300

    tidying things up

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     number = 5
     while number != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.Surface.fill(screen, color=pygame.Color(0,0,0))
+            
+    screen.fill("black")
     pygame.display.flip()
 
 if __name__ == "__main__":
