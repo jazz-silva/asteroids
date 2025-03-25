243b885d0c9fa5c6e865ef449756829e3e465424
José <ja.zzsilva111@gmail.com>
2025-03-24 23:26:50 -0300

    updating files

---
+++ circleshape.py
@@ -0,0 +1,31 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+    def collide(self, circleshape):
+        distance = self.position.distance_to(circleshape.position)
+        total_radius = self.radius + circleshape.radius
+        
+        if distance <= total_radius:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,18 +1,55 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+
+
     number = 5
     while number != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-    screen.fill("black")
-    pygame.display.flip()
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collide(asteroid):
+                print("Game Over")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collide(shot):
+                    asteroid.kill()
+                    shot.kill()
+                    
+        screen.fill("black")
+        for x in drawable:
+            x.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
